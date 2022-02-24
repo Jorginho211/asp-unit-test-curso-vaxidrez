1d536f078b1e55c51d29732931b97b9e5ab7dda4
jorge.lopez <jorge.lopez@tarlogic.com>
2022-02-24 18:48:03 +0100

    Se añade el paquete Moq y se realiza un test para la clase Mockeada ILoggerGeneral

--- LibreriaVaxiNUnitTest/CuentaBancariaNUnitTest.cs
+++ LibreriaVaxiNUnitTest/CuentaBancariaNUnitTest.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using Moq;
+using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,17 +11,21 @@ namespace LibreriaVaxi
     [TestFixture]
     public class CuentaBancariaNUnitTest
     {
-        private CuentaBancaria cuentaBancaria;
-
-        [SetUp]
-        public void Setup()
-        {
-            cuentaBancaria = new CuentaBancaria(new LoggerFake());
-        }
-
         [Test]
         public void Deposto_InputMonto100_ReturnsTrue()
         {
+            CuentaBancaria cuentaBancaria = new CuentaBancaria(new LoggerFake());
+            var resultado = cuentaBancaria.Deposito(100);
+            Assert.IsTrue(resultado);
+            Assert.That(cuentaBancaria.GetBalance(), Is.EqualTo(100));
+        }
+
+        [Test]
+        public void Deposto_InputMonto100Mocking_ReturnsTrue()
+        {
+            var mocking = new Mock<ILoggerGeneral>();
+
+            CuentaBancaria cuentaBancaria = new CuentaBancaria(mocking.Object);
             var resultado = cuentaBancaria.Deposito(100);
             Assert.IsTrue(resultado);
             Assert.That(cuentaBancaria.GetBalance(), Is.EqualTo(100));
